0ae5f4ffcee7083a523cb3315a60a84e2f89947a
Ramprit Sahani <Rampritsahani@outlook.in>
2017-04-17 20:14:15 +0530

    Event Images Added

--- src/app/event/event.service.ts
+++ src/app/event/event.service.ts
@@ -27,7 +27,7 @@ const EVENTS = [
           date: new Date('9/26/2036'),
           time: '10:00 am',
           price: 599.99,
-          imageUrl: '/app/assets/images/angularconnect-shield.png',
+          imageUrl: '/src/assets/images/angularconnect-shield.png',
           location: {
                address: '1057 DT',
                city: 'London',
@@ -105,7 +105,7 @@ const EVENTS = [
           date: new Date('9/26/2036'),
           time: '9:00 am',
           price: 950.00,
-          imageUrl: '/app/assets/images/ng-nl.png',
+          imageUrl: '/src/assets/images/ng-nl.png',
           onlineUrl: 'http://ng-nl.org/',
           sessions: [
                {
@@ -161,7 +161,7 @@ const EVENTS = [
           date: new Date('9/26/2036'),
           time: '9:00 am',
           price: 759.00,
-          imageUrl: '/app/assets/images/ng-conf.png',
+          imageUrl: '/src/assets/images/ng-conf.png',
           location: {
                address: 'The Palatial America Hotel',
                city: 'Salt Lake City',
@@ -243,7 +243,7 @@ const EVENTS = [
           date: new Date('9/26/2036'),
           time: '8:00 am',
           price: 800.00,
-          imageUrl: '/app/assets/images/basic-shield.png',
+          imageUrl: '/src/assets/images/basic-shield.png',
           location: {
                address: 'The UN Angular Center',
                city: 'New York',
@@ -292,7 +292,7 @@ const EVENTS = [
           date: new Date('9/26/2036'),
           time: '9:00 am',
           price: 400.00,
-          imageUrl: '/app/assets/images/ng-vegas.png',
+          imageUrl: '/src/assets/images/ng-vegas.png',
           location: {
                address: 'The Excalibur',
                city: 'Las Vegas',
